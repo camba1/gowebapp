9b0b0ac67aee11fa5c1ac1081595d9a5f5f2b742
Juan Peredo <jperedo@bolbeck.com>
2020-04-20 11:54:44 -0500

    Moved all file management related logic to the new fileManager package

---
+++ fileManager/fileMgr.go
@@ -0,0 +1,117 @@
+package fileManager
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Page: Holds page we need to save/load
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// fileSpec: File system details for a file
+type fileSpec struct {
+	Name     string
+	ModTime  string
+	Size     int64
+	FullName string
+}
+
+//FileListing: List of filenames in a directory
+type FileListing struct {
+	Count     int
+	Names     []string
+	FullNames []string
+	DirName   string
+	Files     []fileSpec
+}
+
+// GetFileName build file name base on tile + the file extension
+func GetFileName(title, filesDir, txtExtension string) (string, error) {
+	if title == "" || txtExtension == "" {
+		return "", errors.New("invalid title or extension")
+	}
+	return filesDir + title + txtExtension, nil
+}
+
+// loadFileListing: Get the list of files in a given directory
+func loadFileListing(dirName string) ([]os.FileInfo, error) {
+	fileListing, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		return nil, err
+	}
+	return fileListing, nil
+}
+
+// ListDirFiles: Load the count and names of the files in a directory into the FileListing struct
+func ListDirFiles(directory, dateFormat string) (FileListing, error) {
+	files := FileListing{DirName: directory}
+	filesInDir, err := loadFileListing(directory)
+	if err != nil {
+		//Do nothing
+	} else {
+		fileSpecs := make([]fileSpec, len(filesInDir))
+		for i, info := range filesInDir {
+			if !info.IsDir() {
+				fileSpecs[i] = fileSpec{
+					FullName: info.Name(),
+					Name:     info.Name()[:len(info.Name())-4],
+					ModTime:  info.ModTime().Format(dateFormat),
+					Size:     info.Size(),
+				}
+			}
+		}
+		files = FileListing{
+			Count:   len(fileSpecs),
+			DirName: directory,
+			Files:   fileSpecs,
+		}
+	}
+	return files, err
+}
+
+//LoadPage: load page from disk into a page struct
+func LoadPage(title string, filesDir, txtExtension string) (*Page, error) {
+	fileName, err := GetFileName(title, filesDir, txtExtension)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, body}, nil
+}
+
+//save: Save a page to disk
+func (pg *Page) Save(filesDir, txtExtension string, fileMode os.FileMode) error {
+	fileName, err := GetFileName(pg.Title, filesDir, txtExtension)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(fileName, pg.Body, fileMode)
+
+}
+
+//saveDisplayTestPage: Save a string into a file. Reload the file and display content.
+func SaveDisplayTestPage(filesDir, txtExtension string, fileMode os.FileMode) {
+	pageTitle := "History"
+	pg := Page{
+		Title: pageTitle,
+		Body:  []byte("This is history!"),
+	}
+	err := pg.Save(filesDir, txtExtension, fileMode)
+	if err != nil {
+		panic(err)
+	}
+	pgLd, err := LoadPage(pageTitle, filesDir, txtExtension)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(pgLd.Title + ": " + string(pgLd.Body))
+	}
+}
--- webApp.go
+++ webApp.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"./fileManager"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 )
-
-// Page: Holds page we need to save/load
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-// fileSpec: File system details for a file
-type fileSpec struct {
-	Name     string
-	ModTime  string
-	Size     int64
-	FullName string
-}
-
-//fileListing: List of filenames in a directory
-type fileListing struct {
-	Count     int
-	Names     []string
-	FullNames []string
-	DirName   string
-	Files     []fileSpec
-}
 
 const dateFormat string = "Jan-02-06"
 
@@ -62,84 +37,9 @@ var templates = template.Must(template.ParseFiles(templatesDir+editTemplate, tem
 
 //validPath: Regular expression to be validate that we have a valid path to save and retrieve files
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+//validFileName: Validate that the file name only contains letters and numbers
 var validFileName = regexp.MustCompile("^([a-zA-Z0-9]+)$")
-
-// getFileName build file name base on tile + the file extension
-func getFileName(title string) string {
-	return filesDir + title + txtExtension
-}
-
-//save: Save a page to disk
-func (pg *Page) save() error {
-	fileName := getFileName(pg.Title)
-	return ioutil.WriteFile(fileName, pg.Body, readWriteFileMode)
-
-}
-
-//loadPage: load page from disk into a page struct
-func loadPage(title string) (*Page, error) {
-	fileName := getFileName(title)
-	body, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{title, body}, nil
-}
-
-//loadFileListing: Get the list of files in a given directory
-func loadFileListing(dirName string) ([]os.FileInfo, error) {
-	fileListing, err := ioutil.ReadDir(dirName)
-	if err != nil {
-		return nil, err
-	}
-	return fileListing, nil
-}
-
-//listDirFiles: Load the count and names of the files in a directory into the fileListing struct
-func listDirFiles(directory string) (fileListing, error) {
-	files := fileListing{DirName: directory}
-	filesInDir, err := loadFileListing(directory)
-	if err != nil {
-		//Do nothing
-	} else {
-		fileSpecs := make([]fileSpec, len(filesInDir))
-		for i, info := range filesInDir {
-			if !info.IsDir() {
-				fileSpecs[i] = fileSpec{
-					FullName: info.Name(),
-					Name:     info.Name()[:len(info.Name())-4],
-					ModTime:  info.ModTime().Format(dateFormat),
-					Size:     info.Size(),
-				}
-			}
-		}
-		files = fileListing{
-			Count:   len(fileSpecs),
-			DirName: directory,
-			Files:   fileSpecs,
-		}
-	}
-	return files, err
-}
-
-//saveDisplayTestPage: Save a string into a file. Reload the file and display content.
-func saveDisplayTestPage() {
-	pageTitle := "History"
-	pg := Page{
-		Title: pageTitle,
-		Body:  []byte("This is history!"),
-	}
-	err := pg.save()
-	if err != nil {
-		panic(err)
-	}
-	pgLd, err := loadPage(pageTitle)
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println(pgLd.Title + ": " + string(pgLd.Body))
-	}
-}
 
 //makeHandler: Take a http request to save/edit/view a file , validate urk, get file title and call the appropriate function
 // Note that we use closures to pass the  function we will need to call at the end of the day as
@@ -158,7 +58,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 //handler: handle http request
 func handler(responseWriter http.ResponseWriter, r *http.Request) {
 	_ = r
-	files, err := listDirFiles(filesDir)
+	files, err := fileManager.ListDirFiles(filesDir, dateFormat)
 	err = templates.ExecuteTemplate(responseWriter, indexTemplate, files)
 	if err != nil {
 		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
@@ -171,7 +71,7 @@ func viewHandler(responseWriter http.ResponseWriter, r *http.Request, title stri
 	//if err != nil {
 	//	return
 	//}
-	pg, err := loadPage(title)
+	pg, err := fileManager.LoadPage(title, filesDir, txtExtension)
 	if err != nil {
 		http.Redirect(responseWriter, r, editUrl+title, http.StatusFound)
 		return
@@ -183,9 +83,9 @@ func viewHandler(responseWriter http.ResponseWriter, r *http.Request, title stri
 //editHandler: Handle request to edit a file
 func editHandler(responseWriter http.ResponseWriter, r *http.Request, title string) {
 	_ = r // added since we still need the handlers to be uniform so this can be called from the makeHandler function
-	pg, err := loadPage(title)
+	pg, err := fileManager.LoadPage(title, filesDir, txtExtension)
 	if err != nil {
-		pg = &Page{Title: title}
+		pg = &fileManager.Page{Title: title}
 	}
 	renderTemplate(responseWriter, pg, editTemplate)
 }
@@ -193,11 +93,11 @@ func editHandler(responseWriter http.ResponseWriter, r *http.Request, title stri
 // saveHandler: Get data from the request body and save it into a new file
 func saveHandler(responseWriter http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	pg := Page{
+	pg := fileManager.Page{
 		Title: title,
 		Body:  []byte(body),
 	}
-	err := pg.save()
+	err := pg.Save(filesDir, txtExtension, readWriteFileMode)
 	if err != nil {
 		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
 	}
@@ -215,7 +115,7 @@ func newHandler(responseWriter http.ResponseWriter, r *http.Request) {
 }
 
 //renderTemplate: Load html template from the template cache and render page content to send back to client
-func renderTemplate(responseWriter http.ResponseWriter, pg *Page, templateName string) {
+func renderTemplate(responseWriter http.ResponseWriter, pg *fileManager.Page, templateName string) {
 	err := templates.ExecuteTemplate(responseWriter, templateName, pg)
 	if err != nil {
 		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
@@ -233,6 +133,6 @@ func startServer() {
 }
 
 func main() {
-	saveDisplayTestPage()
+	fileManager.SaveDisplayTestPage(filesDir, txtExtension, readWriteFileMode)
 	startServer()
 }
